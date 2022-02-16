f36b2f750133a68413e8a7bad72b13ad9f8ca3f7
Nicolas-Tondeur <72604783+Nicolas-Tondeur@users.noreply.github.com>
2022-02-16 16:42:11 +0100

    3ème envoi

--- question4.php
+++ question4.php
@@ -1,8 +1,29 @@
 <?php
-$users = ["Gabriel", "Nicolas", "Mathieu"];
-// $tab_lenght = count($users);
-// echo "le tableau contient " . $tab_lenght ." élements" . "</br>";
+$users = ["Gabriel", "Nicolas", "Mathieu", "Corenthin", "Philippe"];
 foreach($users as $key){
     echo $key ."<br>";
 }
+if(isset($_POST['nom'])){
+    echo $_POST['nom'];
 
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <form method="POST">
+        <label for="nom">Nom :  </label>
+        <input type="str" name="nom">
+        <br>
+    </form>
+</body>
+</html>
